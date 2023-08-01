a12373cb5157bd32dfb1ca9d39f3392c9a1d595d
Z4karia <92750334+Z4karia@users.noreply.github.com>
2023-08-01 17:08:56 +0000

    test: add sp and x tests

--- tests/src/ethereum/swap_normal_flow.test.js
+++ tests/src/ethereum/swap_normal_flow.test.js
@@ -22,16 +22,16 @@ const devices = [
         label: "Nano S",
         steps: 7, // <= Define the number of steps for this test case and this device
     },
-    // {
-    //     name: "nanox",
-    //     label: "Nano X",
-    //     steps: 5, // <= Define the number of steps for this test case and this device
-    // },
-    // {
-    //     name: "nanosp",
-    //     label: "Nano S+",
-    //     steps: 5, // <= Define the number of steps for this test case and this device
-    // }
+    {
+        name: "nanox",
+        label: "Nano X",
+        steps: 5, // <= Define the number of steps for this test case and this device
+    },
+    {
+        name: "nanosp",
+        label: "Nano S+",
+        steps: 5, // <= Define the number of steps for this test case and this device
+    }
 ];
 
 devices.forEach((device) =>
