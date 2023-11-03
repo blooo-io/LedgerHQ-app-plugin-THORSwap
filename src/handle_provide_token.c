b73d0336917fc55ad6acf12742ed51d8e8d3a55f
m4yl1s <149499275+m4yl1s@users.noreply.github.com>
2023-11-03 12:45:16 +0100

    fix(compilation warnings): Fixed compilation warnings with casting

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -43,7 +43,7 @@ void handle_provide_token(void *parameters) {
                 // CAL did not find the token and token is not ETH.
                 context->decimals_received = DEFAULT_DECIMAL;
                 // Use next function to avoid buffer overflow
-                truncate_and_null_terminate_string(DEFAULT_TICKER,
+                truncate_and_null_terminate_string((char *)DEFAULT_TICKER,
                                                    sizeof(context->ticker_received));
                 strlcpy(context->ticker_received, DEFAULT_TICKER, sizeof(context->ticker_received));
                 // We will need an additional screen to display a warning message.
@@ -87,7 +87,7 @@ void handle_provide_token(void *parameters) {
                 // CAL did not find the token and token is not ETH.
                 context->decimals_sent = DEFAULT_DECIMAL;
                 // Use next function to avoid buffer overflow
-                truncate_and_null_terminate_string(DEFAULT_TICKER, sizeof(context->ticker_sent));
+                truncate_and_null_terminate_string((char *)DEFAULT_TICKER, sizeof(context->ticker_sent));
                 strlcpy(context->ticker_sent, DEFAULT_TICKER, sizeof(context->ticker_sent));
                 // We will need an additional screen to display a warning message.
                 msg->additionalScreens++;
